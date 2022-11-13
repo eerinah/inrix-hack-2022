2ded1072b5d768f52702126e5b3ca73afb32a6b3
Eerina Haque <eerinahaque@gmail.com>
2022-11-12 23:04:48 -0800

    removed form

--- webapp/src/components/AddTrip.vue
+++ webapp/src/components/AddTrip.vue
@@ -9,8 +9,7 @@
             Add a trip with a source and destination for transporting your goods. We'll curate the optimal route.
           </h1>
         </v-col>
-
-        <v-col class="mb-5 text-left">
+        <!-- <v-col class="mb-5 text-left">
             <v-btn
                 class="mx-2"
                 fab
@@ -22,7 +21,42 @@
                     mdi-plus
                 </v-icon>
             </v-btn>
-        </v-col>
+        </v-col> -->
+        <form>
+          <!-- <v-text-field
+            class= "mr-15 ml-15"
+            type= "time"
+            v-model="startTime"
+            :rules="startTimeRules"
+            label="Operational Start Hour"
+            required
+            @input="$v.startTime.$touch()"
+            @blur="$v.startTime.$touch()"
+          ></v-text-field>
+
+          <v-text-field
+            class= "mr-15 ml-15"
+            type= "time"
+            v-model="startTime"
+            :rules="startTimeRules"
+            label="Operational End Hour"
+            required
+            @input="$v.startTime.$touch()"
+            @blur="$v.startTime.$touch()"
+          ></v-text-field> -->
+
+          <v-btn
+            class="ml-15 mr-5"
+            @click="submit"
+          >
+            Calculate Route
+          </v-btn>
+          <!-- <v-btn
+            @click="clear"
+            >
+            clear
+          </v-btn> -->
+        </form>
       </v-row>
     </v-container>
   </template>
