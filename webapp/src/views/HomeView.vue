aa3d66eede11c4c933ec4faf015d47d8d36a3ba0
sanfernoronha <sanfernoronha99@gmail.com>
2022-11-12 18:05:48 -0800

    :sparkles: completed gmap test 1

---
+++ webapp/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+
+  <MapTest/>
+  
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import MapTest from '../components/MapTest.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    MapTest
+  }
+}
+</script>
