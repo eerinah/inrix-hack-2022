2992cdceeeb837f9f19313278625b8e4c1753dff
Het Patel <hetp111@users.noreply.github.com>
2022-11-13 02:16:22 -0800

    more update

--- costAnalysis.js
+++ costAnalysis.js
@@ -1,7 +1,7 @@
 //cost(time, fuel(based on weight), distance, traffic) for every path (shortest)
 //time in minutes, distance in miles
 
-function getSchedule(timeDisList){// [[time,dis,s,e], ...], time in hour ([0,24) hour) (eg 11.5 for 11:30), dis in ?
+function getSchedule(timeDisList){// [[time,dis,src,des], ...], time in hour ([0,24) hour) (eg 11.5 for 11:30), dis in ?
     var startTime=8*60;//mins
     var maxTime=-1, maxTimeIndex=-1;
     
@@ -12,11 +12,11 @@ function getSchedule(timeDisList){// [[time,dis,s,e], ...], time in hour ([0,24)
         }
     }
 
-    var finalSchedule;
-    //format: [ [time,distance,startTime,endTime], ...]
+    var finalSchedule = new Array(timeDisList.length);
+    //format: [ [time,distance,startTime,endTime, src, des], ...]
     for(var i=0;i<timeDisList.length;i++){
-        finalSchedule[i] = [ timeDisList[i][0], timeDisList[i][1], startTime+(maxTime-timeDisList[i][0]), startTime+maxTime ];
+        finalSchedule[i] = [ timeDisList[i][0], timeDisList[i][1], startTime+(maxTime-timeDisList[i][0]), startTime+maxTime, timeDisList[i][2], timeDisList[i][3]];
     }
-    
+
     return finalSchedule;
 }
