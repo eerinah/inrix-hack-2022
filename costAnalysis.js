2f98c22ea4b893941e6b7f1626979b46ff5ed380
Het Patel <hetp111@users.noreply.github.com>
2022-11-13 02:54:53 -0800

    min to 24hrs time

--- costAnalysis.js
+++ costAnalysis.js
@@ -3,19 +3,20 @@
 
 function getSchedule(timeDisList){// [[time,dis,src,des], ...], time in hour ([0,24) hour) (eg 11.5 for 11:30), dis in ?
     var startTime=8*60;//mins
-    var maxTime=-1, maxTimeIndex=-1;
+    var maxTime=-1;
     
     for(var i=0;i<timeDisList.length;i++){
         if(maxTime<timeDisList[i][0]){
             maxTime=timeDisList[i][0];
-            maxTimeIndex=i;
         }
     }
 
-    var finalSchedule = new Array(timeDisList.length);
+    var finalSchedule = [];
     //format: [ [time,distance,startTime,endTime, src, des], ...]
     for(var i=0;i<timeDisList.length;i++){
-        finalSchedule[i] = [ timeDisList[i][0], timeDisList[i][1], startTime+(maxTime-timeDisList[i][0]), startTime+maxTime, timeDisList[i][2], timeDisList[i][3]];
+        var st = (startTime+(maxTime-timeDisList[i][0]))/60;
+        var et = (startTime+maxTime)/60;
+        finalSchedule.push([ timeDisList[i][0], timeDisList[i][1], Math.floor(st/60).toString() + int(st%60).toString(), Math.floor(et/60).toString() + int(et%60).toString(), timeDisList[i][2], timeDisList[i][3] ]);
     }
 
     return finalSchedule;
